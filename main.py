118fa44d8b10580b47ef2e36236aa10fc2f03635
maxramirez84 <maxramirez84@gmail.com>
2021-04-12 12:14:43 +0200

    Get all the records from participant rather than only those from the first event where the study number is stored. Reset variables storing data related to the last residency in the ID DCI.

--- main.py
+++ main.py
@@ -26,16 +26,25 @@ if __name__ == '__main__':
     ARG_TO_HF = "testing"
 
     # Health facility specific fields
-    SPECIFIC_FIELDS = ['community', 'other_community', 'address', 'further_contact_details']
+    CONTACT_VARIABLES = ['community', 'other_community', 'address', 'further_contact_details']
 
     project_from = redcap.Project(URL, PROJECTS[ARG_FROM_HF])
     project_to = redcap.Project(URL, PROJECTS[ARG_TO_HF])
 
-    # Get REDCap records of study participant to be migrated
+    # Get REDCap record id of study participant to be migrated
     print("Getting records from {}".format(ARG_STUDY_NUMBER))
     api_filter_logic = "[epipenta1_v0_recru_arm_1][study_number] = '{}'".format(ARG_STUDY_NUMBER)
-    participant_records = project_from.export_records(filter_logic=api_filter_logic)
+    recruitment_records = project_from.export_records(filter_logic=api_filter_logic)
+    record_id = recruitment_records[0]['record_id']
+    print("The record id of participant {} is {}".format(ARG_STUDY_NUMBER, record_id))
+
+    # Get the ALL REDCap records (from every event) of this study participant with the record id
+    participant_records = project_from.export_records(records=record_id)
     print(participant_records)
+
+    # Remove contact details in the ID DCI as they refer to the previous residence
+    for variable in CONTACT_VARIABLES:
+        participant_records[0][variable] = ''
 
     # Import study participant records to the new HF/REDCap project
     print("Importing data from {} to a new record into {}".format(ARG_STUDY_NUMBER, ARG_TO_HF))
